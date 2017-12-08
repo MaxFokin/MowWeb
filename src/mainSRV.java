71e66f35c05d480a22ba9e6c17d4daa0d5a120af
Max Fokin <MaxFokin@gmail.com>
2017-12-08 16:10:41 +0200

    added comments

--- src/mainSRV.java
+++ src/mainSRV.java
@@ -3,10 +3,14 @@ import connection.*;
 public class mainSRV {
 	
 	public static void main(String[] args) {
+		//mySQL Server Connection
 		mysqlConnection sqlConn = new mysqlConnection();
 		sqlConn.setConnection();
 		
+		//SFDC Connection
 		wscConnection sfdcConn = new wscConnection();
 		sfdcConn.setConnection();
+		
+		
 	}
 }
