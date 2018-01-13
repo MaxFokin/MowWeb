a907c7e436002e03fb74a53c5602fdd0f2f378fe
Max Fokin <MaxFokin@gmail.com>
2018-01-13 12:08:49 +0200

    Update Console output message.

--- src/com/MowWeb/services/rest/RSPublisher.java
+++ src/com/MowWeb/services/rest/RSPublisher.java
@@ -16,6 +16,7 @@ public final class RSPublisher {
 		URI baseUri = UriBuilder.fromUri(host).port(port).build();
 		HttpServer server = JdkHttpServerFactory.createHttpServer(baseUri, config);
 		
-		System.out.println("*** RESTful Service Has Started ***\n\t@ http://localhost:8080/rest/");
+		System.out.println("*** RESTful Service Has Started ***\n\t@ " +
+							baseUri + "rest/");
 	}
 }
